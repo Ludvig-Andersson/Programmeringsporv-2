b03bb905be69a344b5db28e77c8fe4ca2da25825
Ludvig-Andersson <Ludvig.andersson@elev.ga.ntig.se>
2021-02-18 14:22:18 +0100

    first commit

---
+++ Programmeringsprov2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Programmeringsporv2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Nocco", "Kyckling", "Äppeljuice", "riskakor", "Bröd" };
+
+
+
+
+            System.Console.WriteLine("Jag behöver hjälp i programering fattar ingenting men ligger efter i andra ämne också");
+
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
